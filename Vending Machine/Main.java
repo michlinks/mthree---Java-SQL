570a74102465661ef9df58793cb197a63e4acad2
michaelalinks <56351494+michlinks@users.noreply.github.com>
2021-11-10 16:04:48 +0000

    Add files via upload

---
+++ Vending Machine/Main.java
@@ -0,0 +1,21 @@
+/*
+* @michaelalinks
+* */
+
+
+package com.company;
+
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+
+            VendingMachine vend = new VendingMachine();
+
+
+    } // end of main method
+
+}// end of Main class
