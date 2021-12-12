255ca048e3faeaa1ab64d4f388c6c0ae2dc6d2bf
michaelalinks <56351494+michlinks@users.noreply.github.com>
2021-12-12 20:29:55 +0000

    Update Main.java

--- 2. DVDLibrary/Project 2 - DVDLibrary/DVDLibrary/src/com/company/Main.java
+++ 2. DVDLibrary/Project 2 - DVDLibrary/DVDLibrary/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String path = "/Users/michaelalinks/Google Drive/Coding/Michaela_Links_Assignment_2/DVDLibrary/src/com/company/dvds.txt";
+        String path = "dvds.txt";
 
         DVDLibrary session = new DVDLibrary(path);
     }
